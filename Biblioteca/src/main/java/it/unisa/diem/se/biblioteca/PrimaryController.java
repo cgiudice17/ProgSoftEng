dbcf40c6bfedfa5f4cf07644d7ecbaa42357f9e7
Fabio Concilio <f.concilio11@studenti.unisa.it>
2025-11-24 16:56:08 +0100

    minor changes

--- Biblioteca/src/main/java/it/unisa/diem/se/biblioteca/PrimaryController.java
+++ Biblioteca/src/main/java/it/unisa/diem/se/biblioteca/PrimaryController.java
@@ -1,12 +1,6 @@
 package it.unisa.diem.se.biblioteca;
 
-import java.io.IOException;
-import javafx.fxml.FXML;
-
 public class PrimaryController {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    
 }
