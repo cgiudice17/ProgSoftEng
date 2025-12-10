8da7caccae29998faa45fbd3e1e103cfd39f9f00
Carmine <c.giudice6@studenti.unisa.it>
2025-12-10 12:51:17 +0100

    modifica commenti

--- Biblioteca/src/main/java/it/unisa/diem/se/biblioteca/App.java
+++ Biblioteca/src/main/java/it/unisa/diem/se/biblioteca/App.java
@@ -9,23 +9,33 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 /**
- * JavaFX App
+ * @brief JavaFX: classe principale dell'applicazione. Questa classe estende Application e gestisce il ciclo di vita dell'interfaccia grafica, l'inizializzazione dello stage
+ * e il caricamente dei file FXML
  */
 public class App extends Application {
 
     private static Scene scene;
-
+    /** @brief Metodo di avvio dell'applicazione JavaFX. Inizializza lo stage primario 
+     *  @param stage fornito dall piattaforma JavaFX
+    */
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
-
+    
+    /**@brief Sostituisce il contenuto della scena attuale caricando un nuovo file FXML.
+     * Questo permette di passare facilmente da una sezione all'altra all'interno dell'applicazione.
+     */
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
+    /**@brief Carica un file FXML e restituisce il nodo genitore
+     *@param nome del file FXML da caricare 
+     *@return il nodo parent 
+     */
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
