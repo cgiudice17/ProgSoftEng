1dec844c3682c773dfaf3099e9eafe98154c9171
Carmine <c.giudice6@studenti.unisa.it>
2025-12-12 18:12:41 +0100

    creazione classi di test

---
+++ Biblioteca/src/test/java/it/unisa/diem/se/biblioteca/author/AuthorTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.unisa.diem.se.biblioteca.author;
+
+/**
+ *
+ * @author giudi
+ */
+public class AuthorTest {
+
+}
