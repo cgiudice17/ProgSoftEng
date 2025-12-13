cef98a71dc8e5d819cbe39adfc76720e7df4f857
Enkate <m.buono40@studenti.unisa.it>
2025-12-13 18:59:41 +0100

    Update Loan class documentation and formatting

--- Biblioteca/src/main/java/it/unisa/diem/se/biblioteca/loan/Loan.java
+++ Biblioteca/src/main/java/it/unisa/diem/se/biblioteca/loan/Loan.java
@@ -11,17 +11,14 @@ public class Loan implements Comparable<Loan>, Serializable{
     private Book book;
     private LocalDate returnDate;
     
-    /**
-     * @brief Costruttore di default
-     * 
-     * @param[in] user
-     * @param[in] book
-     * @param[in] returnDate 
-     * 
-     * 
-     * @post Prestito inizializzato correttamente
+     /**
+     * @brief Costruttore parametrico della classe Loan.
+     *
+     * @param user          L'utente che ha richiesto il prestito.
+     * @param book          Il libro oggetto del prestito.
+     * @param returnDate    La data prevista per la restituzione.
+     * @post Il prestito è inizializzato correttamente con i dati forniti.
      */
-    
     public Loan(User user, Book book, LocalDate returnDate){
         this.user = user;
         this.book = book;
@@ -53,20 +50,23 @@ public class Loan implements Comparable<Loan>, Serializable{
     }
     
     
-    
     /**
-     * @brief Override del metodo compareTo che confronta i prestiti 
-     *        in base al data di restituzione
-     * @param l
-     * @return Un intero negativo se l'ogetto chiamate viene prima cronologicamente dell'argomento,
-     *         0 se sono uguali e un intero positivo se viene dopo.
+     * @brief Confronta due prestiti in base alla data di restituzione
+     * @param l       Il prestito da confrontare.
+     * @return Un intero negativo se l'ogetto chiamate viene prima cronologicamente dell'argomento, 0 se le date sono uguali, e un intero positivo se viene dopo
      */
-
     @Override
     public int compareTo(Loan l) {
         return this.returnDate.compareTo(l.returnDate);
     }
-        
+    /**
+     * @brief Verifica l'uguaglianza tra questo prestito e un altro oggetto.
+     * Due prestiti sono considerati uguali se coinvolgono lo stesso utente,
+     * lo stesso libro e hanno la stessa data di restituzione prevista.
+     *
+     * @param o      L'oggetto con cui confrontare il prestito
+     * @return ritorna true se i prestiti sono uguali, altrimenti false
+     */
     @Override
     public boolean equals(Object o){
         if(o == null || !this.getClass().equals(o.getClass())) return false;
@@ -74,10 +74,10 @@ public class Loan implements Comparable<Loan>, Serializable{
         Loan l = (Loan) o;
         return this.book.equals(l.getBook()) && this.returnDate.equals(l.getReturnDate()) && this.user.equals(l.getUser());
     }
-
-     /**
-     * @brief Override del metotdo toString per stampare la classe corrente.
-     */
+    /**
+     * @brief Restituisce una rappresentazione in formato stringa del prestito
+     *@return ritorna una stringa formattata con Utente, Titolo del Libro e Data di restituzione
+     **/
     @Override
     public String toString(){
         return "Prestito: Utente: " + user + ", Libro: " + book.getTitle() + ", Data: " + returnDate;
